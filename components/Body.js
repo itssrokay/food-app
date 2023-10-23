f83eb87fda186ebd7662e183654ab8127dfeca24
Shivam raj <shivamraj82715@gmail.com>
2023-10-23 16:54:25 +0530

    multiple search at a time

--- components/Body.js
+++ components/Body.js
@@ -3,58 +3,55 @@ import Data from "./Data";
 import Cards from "./Cards";
 
 export default function Body() {
-  const [searchInput,setSearchInput]=useState("");
-  const[restaurants,setRestaurants]=useState(Data);
+  const [searchInput, setSearchInput] = useState("");
+  const [restaurants, setRestaurants] = useState(Data);
+const newArray=[];
+newArray.push(...Data);
+  
 
-  function filterRes(searchInput,restaurants){
-    const filterData=restaurants.filter((restaurant)=>
-    restaurant.data.name.includes(searchInput)
+  function filterRes(searchInput, newArray) {
+    const filterData = newArray.filter((restaurant) =>
+      restaurant.data.name.includes(searchInput)
     );
     return filterData;
-   
   }
 
   const elem = restaurants.map((item) => {
     return (
       <>
-      <div className="dir">
-        <Cards
-          url={item.data?.url}
-          name={item.data.name}
-          key={item.data.id}
-          area={item.data.area}
-        />
-      </div>
+        <div className="dir">
+          <Cards
+            url={item.data?.url}
+            name={item.data.name}
+            key={item.data.id}
+            area={item.data.area}
+          />
+        </div>
       </>
     );
   });
 
-
-  
-
   return (
     <>
-    <div className="search-container">
+      <div className="search-container">
         <input
-         type="text"
-         className="search-input"
-         placeholder="Search"
-         value={searchInput}
-         onChange={(e)=>
-          setSearchInput(e.target.value)
-           }
-         />
-        
-         <button 
-         className="search-btn" 
-         onClick={()=>
-          {
-           const resultant= filterRes(searchInput,restaurants);
-           setRestaurants(resultant)
-          }
-          }>Bang</button>
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const resultant = filterRes(searchInput, newArray);
+            setRestaurants(resultant);
+          }}
+        >
+          Bang
+        </button>
       </div>
-    <div className="cards">{elem}</div>
+      <div className="cards">{elem}</div>
     </>
   );
 }
