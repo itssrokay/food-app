2c7a57aaed60a3877ed39a468fe64b88aee5899b
itssrokay <shivamraj82715@gmail.com>
2024-01-12 18:08:57 +0530

    used React router, useRouteError!

--- App.js
+++ App.js
@@ -6,17 +6,45 @@ import Heading from "./components/Heading";
 import Footer from "./components/Footer";
 import Body from "./components/Body";
 import Data from "./components/Data";
-
+import { createBrowserRouter,RouterProvider ,Outlet} from "react-router-dom";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Error from "./components/Error";
+import { Link } from "react-router-dom";
 const App = () => {
   return (
     <>
       <Heading />
-      <Body />
-      <Footer />
+      <Outlet/>
     </>
   );
 };
+
+const appRouter=createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+      {
+        path:"/",
+        element:<Body/>
+      },
+      {
+        path:"/about",
+        element:<About/>
+      },
+      {
+        path:"/contact",
+        element:<Contact/>
+      },
+    ],
+    errorElement:<Error/>,
+  },
+ 
+])
+
+
 // ReactDOM.render(<App />, document.getElementById('root'));
 const root = createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
--- components/Heading.js
+++ components/Heading.js
@@ -1,24 +1,32 @@
-import React from "react"
+// Heading.js
 
-const Title=()=>(
-  <a href="/">
-<img src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj" alt="logo" className="logo"/>
-</a>
+import React from "react";
+import { Link } from "react-router-dom";
 
-)
+const Title = () => (
+  <Link to="/">   
+    <img
+      src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
+      alt="logo"
+      className="logo"
+    />
+  </Link>
+);
 
 export default function Heading() {
   return (
     <div className="head">
-    <Title/>
+      <Title />
       <h1>Food villa</h1>
-    <div className="navbar">
-      <ul >
-   <li>Home</li>
-   <li>Menu</li>
-      <li>contact us</li>
-      </ul>
+      <div className="navbar">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li>
+            <Link to="/contact">contact us</Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
-  };
+}
